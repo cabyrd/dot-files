2f1ceb05540bd993e41a575fd8fd321e5e316c59
Chris Byrd <byrdca@gmail.com>
2018-09-25 18:22:48 -0700

    Remove link script in favor of yadm

--- link-dot-files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-LINK_ABS_PATH=`realpath $0`
-DOT_PATH=`dirname $LINK_ABS_PATH`
-ln -sf ${DOT_PATH}/.bashrc ~/.bashrc
-ln -sf ${DOT_PATH}/.vimrc ~/.vimrc
-ln -sf ${DOT_PATH}/.tmux.conf ~/.tmux.conf
