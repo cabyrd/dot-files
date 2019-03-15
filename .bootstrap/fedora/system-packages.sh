0c3e058884041bf53d8ae64e9dfbdbeb4b7f7d95
Chris Byrd <byrdca@gmail.com>
2019-03-15 11:40:34 -0700

    update fedora setup

--- .bootstrap/fedora/system-packages.sh
+++ .bootstrap/fedora/system-packages.sh
@@ -4,6 +4,7 @@
 # Install basic system packages
 # -----------------------------------------------------------------------------
 sudo dnf install -y bash-completion     \
+                    buildah             \
                     ctags               \
                     curl                \
                     direnv              \
@@ -11,10 +12,13 @@ sudo dnf install -y bash-completion     \
                     gcc-c++             \
                     golang              \
                     gpg                 \
+                    keepassxc           \
                     make                \
                     patch               \
                     pipenv              \
+                    podman              \
                     powerline           \
+                    restic              \
                     task                \
                     the_silver_searcher \
                     tmux                \
